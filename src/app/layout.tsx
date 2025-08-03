6c818d59fd728af565e30cf2a5b7c698d1003320
zeteoh <teohzeloong12@gmail.com>
2025-08-03 22:11:55 +0800

    ui: added barebone sidebar with mobile layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans dark`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,36 @@
 import {
   Sidebar,
+  SidebarContent,
+  SidebarFooter,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { AppSidebarClient } from "./_AppSidebarClient";
 
 export default function HomePage() {
   return (
     <SidebarProvider className="overflow-y-hidden">
-      <Sidebar collapsible="icon" className="overflow-y-hidden">
-        <SidebarHeader className="flex-row">
-          <SidebarTrigger />
-          <span className="text-xl text-nowrap">WDS Jobs</span>
-        </SidebarHeader>
-      </Sidebar>
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-y-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">WDS Jobs</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>hehe</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">adadadadda</main>
+      </AppSidebarClient>
     </SidebarProvider>
   );
 }
