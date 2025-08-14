e3fe6ab6f22fa967497b77fab1339a3b54a450c5
zeteoh <teohzeloong12@gmail.com>
2025-08-14 21:51:03 +0800

    init: inngest for app

---
+++ src/app/api/inngest/route.ts
@@ -0,0 +1,10 @@
+import { inngest } from "@/services/inngest/client";
+import { serve } from "inngest/next";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    /* your functions will be passed here later! */
+  ],
+});
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/", "/api(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
